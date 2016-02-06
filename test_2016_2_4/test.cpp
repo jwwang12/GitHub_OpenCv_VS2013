3c321a80472b24dbc3d834c12d5df2d15d9a4637
jwwang12 <877150341@qq.com>
2016-02-06 20:24:54 +0800

    for the ling

---
+++ test_2016_2_4/test.cpp
@@ -0,0 +1,27 @@
+//显示图像文件
+#include <opencv2/opencv.hpp>
+using namespace std;
+
+#pragma comment(linker, "/subsystem:\"windows\" /entry:\"mainCRTStartup\"")
+
+int main()
+{
+	const char *pstrImageName = "learn2.JPG";
+	const char *pstrWindowsTitle = "OpenCV第一个程序";
+
+	//从文件中读取图像
+	IplImage *pImage = cvLoadImage(pstrImageName, CV_LOAD_IMAGE_UNCHANGED);
+
+	//创建窗口
+	cvNamedWindow(pstrWindowsTitle, CV_WINDOW_AUTOSIZE);
+
+	//在指定窗口中显示图像
+	cvShowImage(pstrWindowsTitle, pImage);
+
+	//等待按键事件
+	cvWaitKey();
+
+	cvDestroyWindow(pstrWindowsTitle);
+	cvReleaseImage(&pImage);
+	return 0;
+}
